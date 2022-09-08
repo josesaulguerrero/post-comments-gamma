ef2dea5327dc7d34e824201997c980382ed32361
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-09-08 10:57:37 -0500

    Add websocket on the main controller

--- src/main/java/co/com/post_comments/gamma/application/bus/RabbitMQConsumer.java
+++ src/main/java/co/com/post_comments/gamma/application/bus/RabbitMQConsumer.java
@@ -1,29 +1,30 @@
-package co.com.post_comments.gamma.application.config;
+package co.com.post_comments.gamma.application.bus;
 
 import co.com.post_comments.gamma.application.bus.views.CommentView;
 import co.com.post_comments.gamma.application.bus.views.PostView;
 import co.com.post_comments.gamma.application.commons.json.JSONMapper;
+import co.com.post_comments.gamma.application.sockets.MainSocket;
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 @AllArgsConstructor
-public class RabbitMQConfig {
+public class RabbitMQConsumer {
     public static final String PROXY_QUEUE_POST_CREATED = "events.proxy.post.created";
     public static final String PROXY_QUEUE_COMMENT_ADDED = "events.proxy.comment.added";
-    
     private final JSONMapper jsonMapper;
+    private final MainSocket socket;
 
-    @RabbitListener(queues = { PROXY_QUEUE_POST_CREATED })
+    @RabbitListener(queues = {PROXY_QUEUE_POST_CREATED})
     public void postCreatedRabbitListener(String jsonView) {
         PostView view = (PostView) this.jsonMapper.readFromJson(jsonView, PostView.class);
-        System.out.println("post view = " + view);
+        this.socket.sendPostCreated("main space", view);
     }
 
-    @RabbitListener(queues = { PROXY_QUEUE_COMMENT_ADDED })
+    @RabbitListener(queues = {PROXY_QUEUE_COMMENT_ADDED})
     public void commendAddedRabbitListener(String jsonView) {
         CommentView view = (CommentView) this.jsonMapper.readFromJson(jsonView, CommentView.class);
-        System.out.println("comment view = " + view);
+        this.socket.sendCommentAdded(view.id(), view);
     }
 }
--- src/main/java/co/com/post_comments/gamma/application/config/WebSocketConfig.java
+++ src/main/java/co/com/post_comments/gamma/application/config/WebSocketConfig.java
@@ -1,7 +1,13 @@
 package co.com.post_comments.gamma.application.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
 public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
 }
