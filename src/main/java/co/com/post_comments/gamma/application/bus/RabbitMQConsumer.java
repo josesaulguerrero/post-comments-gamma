51ec1bda38d0f1a0fc7106210ad232a585504768
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-09-15 21:05:22 -0500

    Add logs on the rabbit listener and socket controller

--- src/main/java/co/com/post_comments/gamma/application/bus/RabbitMQConsumer.java
+++ src/main/java/co/com/post_comments/gamma/application/bus/RabbitMQConsumer.java
@@ -5,11 +5,13 @@ import co.com.post_comments.gamma.application.bus.views.PostView;
 import co.com.post_comments.gamma.application.commons.json.JSONMapper;
 import co.com.post_comments.gamma.application.sockets.Socket;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @AllArgsConstructor
+@Slf4j
 public class RabbitMQConsumer {
     public static final String PROXY_QUEUE_POST_CREATED = "events.proxy.post.created";
     public static final String PROXY_QUEUE_COMMENT_ADDED = "events.proxy.comment.added";
@@ -18,12 +20,14 @@ public class RabbitMQConsumer {
 
     @RabbitListener(queues = {PROXY_QUEUE_POST_CREATED})
     public void postCreatedRabbitListener(String jsonView) {
+        log.info("Received a PostView with the following information " + jsonView + " from the BETA queue.");
         PostView view = (PostView) this.jsonMapper.readFromJson(jsonView, PostView.class);
         this.socket.emitPostCreated(view);
     }
 
     @RabbitListener(queues = {PROXY_QUEUE_COMMENT_ADDED})
     public void commendAddedRabbitListener(String jsonView) {
+        log.info("Received a CommentView with the following information " + jsonView + " from the BETA queue.");
         CommentView view = (CommentView) this.jsonMapper.readFromJson(jsonView, CommentView.class);
         this.socket.emitCommentAdded(view);
     }
--- src/main/java/co/com/post_comments/gamma/application/sockets/Socket.java
+++ src/main/java/co/com/post_comments/gamma/application/sockets/Socket.java
@@ -3,19 +3,23 @@ package co.com.post_comments.gamma.application.sockets;
 import co.com.post_comments.gamma.application.bus.views.CommentView;
 import co.com.post_comments.gamma.application.bus.views.PostView;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+@Slf4j
 @Controller
 @AllArgsConstructor
 public class Socket {
     private SimpMessagingTemplate template;
 
     public void emitPostCreated(PostView view) {
+        log.info("Emitting a PostView to the post.created socket topic.");
         this.template.convertAndSend("/topic/post.created", view);
     }
 
     public void emitCommentAdded(CommentView view) {
+        log.info("Emitting a CommentView to the comment.added socket topic.");
         this.template.convertAndSend(
                 String.format("/topic/%s/comment.added", view.getPostId()),
                 view
